c0a3849843adc94c553762eafd56751d9f67b90a
user <user@PC>
2023-02-26 12:11:06 -0500

    Add project files.

---
+++ AbstractAnimalClass/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace animalclassawesomewin
+{
+    abstract class Animal
+    {
+        // Property
+        public abstract string Name { get; set; }
+        // Methods
+        public abstract string Describe();
+        public string whatAmI()
+        {
+            return "I am an animal";
+        }
+    }
+
+    class Dog : Animal
+    {
+        public override string Name { get; set; }
+        public string Description;
+        public string Type;
+
+        public Dog()
+        {
+            Name = string.Empty;
+            Type = string.Empty;
+            Description = string.Empty;
+        }
+        public Dog(string name, string description, string type)
+        {
+            Name = name;
+            Description = description;
+            Type = type;
+        }
+        public override string Describe()
+        {
+            return $"[ ! ] Data Display [ ! ]\n" +
+                $"{Name}\n" +
+                $"{Description}\n" +
+                $"{Type}";
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog dogOne = new Dog();
+            Dog dogTwo = new Dog("dogNAME", "dogDESCRIPTION", "dogTYPE");
+            Console.WriteLine(dogOne.Describe());
+            Console.WriteLine(dogTwo.Describe());
+        }
+    }
+}
